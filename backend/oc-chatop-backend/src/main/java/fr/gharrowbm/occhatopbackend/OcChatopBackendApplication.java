9da3b86b523f3e39cc5464c0fb1550aef1a045dc
gharrowbm <gharrow.games@gmail.com>
2025-02-07 16:50:52 +0100

    Génération du projet avec les dépendances adéquates

---
+++ backend/oc-chatop-backend/src/main/java/fr/gharrowbm/occhatopbackend/OcChatopBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.gharrowbm.occhatopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcChatopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcChatopBackendApplication.class, args);
+    }
+
+}
