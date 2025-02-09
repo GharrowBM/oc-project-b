806fc29ac286b55c050775122b6ac1c7d5cdfbcf
gharrowbm <gharrow.games@gmail.com>
2025-02-09 10:12:08 +0100

    MaJ de la version de Swagger dans les dépendances

--- backend/oc-chatop-backend/src/main/java/fr/gharrowbm/occhatopbackend/controllers/AuthController.java
+++ backend/oc-chatop-backend/src/main/java/fr/gharrowbm/occhatopbackend/controllers/AuthController.java
@@ -3,7 +3,6 @@ package fr.gharrowbm.occhatopbackend.controllers;
 import fr.gharrowbm.occhatopbackend.models.*;
 import fr.gharrowbm.occhatopbackend.services.ChatopUserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
